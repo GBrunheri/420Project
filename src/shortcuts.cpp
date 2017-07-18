5aff8ae170ca303e196c8365e5174a545df307f7
dikbutt <lokinhocraft@gmail.com>
2017-07-18 20:53:41 -0300

    vaotmaomsaod

---
+++ src/shortcuts.cpp
@@ -0,0 +1,18 @@
+#include "shortcuts.h"
+
+void Shortcuts::PollEvent(SDL_Event* event)
+{
+	if (((event->key.keysym.sym == SDLK_INSERT && event->type == SDL_KEYDOWN)) || ((event->key.keysym.mod & KMOD_LALT) && event->key.keysym.sym == SDLK_i && event->type == SDL_KEYDOWN))
+		UI::SetVisible(!UI::isVisible);
+	if (event->type == SDL_MOUSEBUTTONDOWN && event->button.button == SDL_BUTTON_RIGHT)
+		UI::QuickToggle();
+	
+}
+
+void Shortcuts::SetKeyCodeState(ButtonCode_t code, bool bPressed)
+{
+#ifdef EXPERIMENTAL_SETTINGS
+	if (inputSystem->IsButtonDown(ButtonCode_t::KEY_LALT) && code == ButtonCode_t::KEY_F && bPressed)
+		Settings::ThirdPerson::enabled = !Settings::ThirdPerson::enabled;
+#endif
+}
